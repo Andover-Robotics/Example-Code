eedb82ef4c6ade1db39d3a02f78f158aa10ba623
darcn <darcn@users.noreply.github.com>
2015-09-05 16:05:01 -0400

    Create Application.java

---
+++ src/example/tutorial6/Application.java
@@ -0,0 +1,13 @@
+
+public class Application {
+
+  public static double getHypotenuse(double x, double y) {
+    return Math.sqrt(x * x + y * y);
+  }
+
+  public static void main(String[] args) {
+    //This tutorial will be about functions
+    //Functions are a useful way when programming to seperate code into seperate routines for later code-reuse
+  }
+
+}
