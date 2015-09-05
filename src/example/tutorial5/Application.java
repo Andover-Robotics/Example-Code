9c553d6a6fd312b188b90d467624bbf16c2bc8e2
darcn <darcn@users.noreply.github.com>
2015-09-05 00:04:08 -0400

    Control Structures

---
+++ src/example/tutorial5/Application.java
@@ -0,0 +1,11 @@
+
+public class Application {
+
+  public static void main(String[] args) {
+    //In this tutorial I will show you a few fundamental control structures, including
+    //      if, else if, else
+    //      while loops
+    //      functions
+  }
+
+}
